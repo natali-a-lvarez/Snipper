ed0b535d45cc264f7cab530a860ca388d172c722
natali-a-lvarez <03nataliaalvarez07@gmail.com>
2024-04-18 14:09:25 -0400

    creating encryption for code in post request

--- app/main.py
+++ app/main.py
@@ -2,7 +2,21 @@
 from flask import request, jsonify
 from config import app,db
 from models import Snippet
+from cryptography.fernet import Fernet
+from os import environ as env
+from dotenv import find_dotenv, load_dotenv
 
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+    
+app.secret_key = env.get("FERNET_KEY")
+
+fern = Fernet(app.secret_key)
+
+# def decrypt_code(data):
+#     data['code'] = fern.decrypt(data['code']).decode()
+#     return data
 
 #GET ALL
 @app.route('/snippet', methods={"GET"})
@@ -12,7 +26,6 @@ def get_snippets():
     # converting to json since it is an object
     json_snippets = list(map(lambda x: x.to_json(), snippets))
     return jsonify({"snippets": json_snippets}), 200
-
 
 
 # GET by ID
@@ -25,10 +38,14 @@ def get_snippet(id):
     
     return jsonify(snippet.to_json()), 200 
 
-# Creaate a recipe
+# Create a recipe
 @app.route('/snippet', methods=['POST'])
 def create_snippet():
     data = request.json
+
+    # encrypt code 
+    data['code'] = fern.encrypt(bytes(data['code'], encoding='utf-8')).decode()
+
     new_snippet = Snippet(language=data['language'], code=data['code'])
 
     # adding it to our model
